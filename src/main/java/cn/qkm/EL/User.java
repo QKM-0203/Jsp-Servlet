465db44f35998a48c394549dfa89c9fe1e942d8b
QKM-0203 <com_qkm@xiyoulinux.org>
2021-01-18 21:27:32 +0800

    update

---
+++ src/main/java/cn/qkm/EL/User.java
@@ -0,0 +1,22 @@
+package cn.qkm.EL;
+
+public class User {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
